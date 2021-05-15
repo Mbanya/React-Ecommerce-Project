40a02e0b95210189a3a7405507fad8a1a62dfe86
mbanya <mbanyaw@gmail.com>
2021-05-15 23:47:44 +0300

    Done Navbar,Products and connected CommerceJs

---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,54 @@
+import {makeStyles,fade} from "@material-ui/core";
+
+const drawerWidth = 0;
+
+export default makeStyles((theme)=>({
+    appBar: {
+        boxShadow: 'none',
+        borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% ~ ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+        },
+    },
+    title: {
+        flexGrow: 1,
+        alignItems: 'center',
+        display: 'flex',
+        textDecoration: 'none',
+    },
+    image: {
+        marginRight: '10px',
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    search: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]:{
+            width: '20ch',
+        },
+    },
+}));
